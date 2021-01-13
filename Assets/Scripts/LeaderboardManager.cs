e99d5e3a2f1ad6d96021d6c0372a195d23a1eeb8
AugustDG <33916325+AugustDG@users.noreply.github.com>
2021-01-12 22:47:46 -0500

    Added leaderboard code
    Basic mongodb code with specific .dlls

--- Assets/Scripts/LeaderboardManager.cs
+++ Assets/Scripts/LeaderboardManager.cs
@@ -1,6 +1,16 @@
-﻿using UnityEngine;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using UnityEngine;
 
 public class LeaderboardManager : MonoBehaviour
 {
-    
+    public void Awake()
+    {
+        MongoClient.Instance = new MongoClient();
+    }
+
+    public void Start()
+    {
+        //todo: add leaderboard code!
+    }
 }
