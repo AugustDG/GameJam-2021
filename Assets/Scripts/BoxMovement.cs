abed51faeb81ddc708bbd5255b5d51b38897db18
Mika24boss <77291451+Mika24boss@users.noreply.github.com>
2021-01-12 23:58:39 -0500

    Basic box sorting
    Functional box sorting, totally not a dick

--- Assets/Scripts/BoxMovement.cs
+++ Assets/Scripts/BoxMovement.cs
@@ -4,7 +4,11 @@ using UnityEngine;
 
 public class BoxMovement : MonoBehaviour
 {
-    public Trigger_CSplitter splitter;
+    public Trigger_CSplitter splitterTrigger;
+    public BoxCollider2D splitterCollider;
+    public GameObject box;
+    public BoxCollider2D boxCollider;
+
     public int direction = 2; //0 up, 1 right, 2 bottom, 3 left
     public Rigidbody2D m_Rigidbody2D;
     public float boxSpeed = 10f;
@@ -12,21 +16,28 @@ public class BoxMovement : MonoBehaviour
     private Vector3 m_Velocity = Vector3.zero;
     
     public bool isDecided = false;
+    public bool facingLeft = false;
 
     void Update()
     {
-        if (splitter.isActivated)
+        if (!isDecided)
         {
-            //checker si le highlight est on, s'il ne l'est pas, on le turn on, et apres on fait un else (s'il n'est pas activé) et on desactive SI il est activé
-
-
-            if (Input.GetButtonDown("FireGood"))
+            if (splitterTrigger.isActivated)
             {
-
-            }
-            else if (Input.GetButtonDown("FireBad"))
-            {
-
+                if (box.transform.position.y <= 7)
+                //checker si le highlight est on, s'il ne l'est pas, on le turn on, et apres on fait un else (s'il n'est pas activé) et on desactive SI il est activé
+                {
+                    if (Input.GetButtonDown("FireGood"))
+                    {
+                        isDecided = true;
+                        direction = 3;
+                    }
+                    else if (Input.GetButtonDown("FireBad"))
+                    {
+                        isDecided = true;
+                        direction = 1;
+                    }
+                }
             }
         }
     }
@@ -34,21 +45,37 @@ public class BoxMovement : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        Vector3 targetVelocity = new Vector2(0, 0);
-        switch (direction)
+
+        Vector3 targetVelocity = new Vector2(0, -boxSpeed * 10f);
+        m_Rigidbody2D.constraints = RigidbodyConstraints2D.None;
+
+        if (isDecided)
         {
-            case 0:
-                targetVelocity = new Vector2(0, boxSpeed * 10f);
-                break;
-            case 1:
-                targetVelocity = new Vector2(boxSpeed * 10f, 0);
-                break;
-            case 2:
-                targetVelocity = new Vector2(0, -boxSpeed * 10f);
-                break;
-            case 3:
-                targetVelocity = new Vector2(-boxSpeed * 10f, 0);
-                break;
+            int tempDirection = direction;
+            if (box.transform.position.x >= 0.75 && box.transform.position.x <= 1.25 && box.transform.position.y > 6)
+            {
+                tempDirection = 2;
+            }
+
+            switch (tempDirection)
+            {
+                case 0:
+                    targetVelocity = new Vector2(0, boxSpeed * 10f);
+                    break;
+                case 1:
+                    targetVelocity = new Vector2(boxSpeed * 10f, 0);
+                    break;
+                case 2:
+                    targetVelocity = new Vector2(0, -boxSpeed * 10f);
+                    break;
+                case 3:
+                    targetVelocity = new Vector2(-boxSpeed * 10f, 0);
+                    break;
+            }
+        }
+        else if (!isDecided && box.transform.position.y <= 6.75)
+        {
+            m_Rigidbody2D.constraints = RigidbodyConstraints2D.FreezePositionX | RigidbodyConstraints2D.FreezePositionY;
         }
         m_Rigidbody2D.velocity = Vector3.SmoothDamp(m_Rigidbody2D.velocity, targetVelocity, ref m_Velocity, m_MovementSmoothing);
     }
