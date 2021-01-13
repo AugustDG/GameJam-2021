432420963fb3ea40e3ab2eba7a23a74289a3f9e4
AugustDG <33916325+AugustDG@users.noreply.github.com>
2021-01-12 19:21:36 -0500

    MainMenu added
    Organisé les scripts et ajouté un main menu basique.

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        SceneManager.LoadSceneAsync(1);
+    }
+
+    public void QuitGame()
+    {
+#if UNITY_EDITOR
+        EditorApplication.ExitPlaymode();
+#else
+        Application.Quit();
+#endif
+    }
+}
