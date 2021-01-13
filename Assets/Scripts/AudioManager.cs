76b884335f7716be8d1343d092b05fa251e763e9
AugustDG <33916325+AugustDG@users.noreply.github.com>
2021-01-13 09:16:53 -0500

    Ajouté les soundtracks!

--- Assets/Scripts/AudioManager.cs
+++ Assets/Scripts/AudioManager.cs
@@ -1,18 +1,37 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.SceneManagement;
 using UnityEngine;
 
+[RequireComponent(typeof(AudioSource))]
 public class AudioManager : MonoBehaviour
 {
+    public AudioClip mainMenuClip;
+    public AudioClip gameMenuClip;
+    public AudioClip bossMenuClip;
+
+    private AudioSource _source;
+    
+    public void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+
+        GameEvents.MainSceneLoading += MainSceneLoadingHandler;
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        _source = GetComponent<AudioSource>();
+
+        _source.clip = mainMenuClip;
+        _source.Play();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void MainSceneLoadingHandler(object sender, EventArgs args)
     {
-        
+        _source.clip = gameMenuClip;
+        _source.Play();
     }
 }
